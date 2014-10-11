e7c61aa3097c3fc50e7bdafc00406239f93f8840
bfisch02 <brett.fischler@tufts.edu>
2014-10-11 17:38:37 -0400

    Added constructors for AccessPoint classes

--- app/src/main/java/com/tylerlubeck/buildingmapper/AccessPoint.java
+++ app/src/main/java/com/tylerlubeck/buildingmapper/AccessPoint.java
@@ -1,7 +1,30 @@
 package com.tylerlubeck.buildingmapper;
 
+import java.util.Date;
+
 /**
- * Created by brettfischler on 10/11/14.
+ * Created by brettfischlerandkennethwapman on 10/11/14.
  */
 public class AccessPoint {
+    private String bssid;
+    private double rss;
+    private Date date;
+    private double std_dev;
+    private int loc_id;
+
+    AccessPoint(String bssid, double rss, Date date)
+    {
+        this.bssid = bssid;
+        this.rss = rss;
+        this.date = date;
+        this.std_dev = -1;
+        this.loc_id = -1;
+    }
+
+    void save(int loc_id, double std_dev)
+    {
+        this.loc_id = loc_id;
+        this.std_dev = std_dev;
+        // Save to table
+    }
 }
